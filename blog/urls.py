01acb0765b0de8f516f7f868a5c41fcba1806dba
Orlando Jin <lejin@ebay.com>
2015-06-30 21:47:04 -0700

    fix the merge issue

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list),
-<<<<<<< HEAD
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
-=======
->>>>>>> 550d02350d5da4128831dfedd334ab4b3df9e70f
 ]
